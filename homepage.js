44a4d69545eb5319bfe7e2a01bafa42885698f6d
Parva2603 <79582206+Parva2603@users.noreply.github.com>
2025-04-15 20:47:02 +0530

    Update homepage.js

--- homepage.js
+++ homepage.js
@@ -1,7 +1,4 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
-const supabaseUrl = "https://jzcmbrqogyghyhvwzgps.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp6Y21icnFvZ3lnaHlodnd6Z3BzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQyMjA0MzUsImV4cCI6MjA1OTc5NjQzNX0.2Cc_QdoHU_Q72XFKmhRD0bvOnx4t2UswWy5lK_w8aj4";
-const supabaseClient = createClient(supabaseUrl, supabaseKey);
+import { supabaseClient } from './supabase.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   fetchFeaturedAuctions();
