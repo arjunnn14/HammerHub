93552cc42a9f4ae8ccda7808a983a50a0c40bf1f
Parva2603 <79582206+Parva2603@users.noreply.github.com>
2025-04-15 23:39:46 +0530

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -5,11 +5,6 @@ const supabaseUrl = "https://jzcmbrqogyghyhvwzgps.supabase.co";
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp6Y21icnFvZ3lnaHlodnd6Z3BzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQyMjA0MzUsImV4cCI6MjA1OTc5NjQzNX0.2Cc_QdoHU_Q72XFKmhRD0bvOnx4t2UswWy5lK_w8aj4";
 export const supabaseClient = createClient(supabaseUrl, supabaseKey);
 
-import { createClient } from '@supabase/supabase-js';
-
-// Initialize Supabase client
-const supabase = createClient('YOUR_SUPABASE_URL', 'YOUR_SUPABASE_API_KEY');
-
 // Function to update status for all auctions
 async function updateAllAuctionStatuses() {
   try {
